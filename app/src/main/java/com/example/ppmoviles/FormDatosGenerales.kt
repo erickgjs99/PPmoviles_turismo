74048045d89e81faaad72eae3f3875bd46aff3ce
Erick Gonzalo Jaramillo Soto <erickjs99lol@gmail.com>
2021-12-28 17:52:53 -0500

    formulario datos generales

---
+++ app/src/main/java/com/example/ppmoviles/FormDatosGenerales.kt
@@ -0,0 +1,13 @@
+package com.example.ppmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormDatosGenerales : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_datos_generales)
+    }
+}
