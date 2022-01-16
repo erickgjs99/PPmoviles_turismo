c499bb6b391db64c4bbb71821d560c7c0749c645
Erick Gonzalo Jaramillo Soto <erickjs99lol@gmail.com>
2022-01-16 18:32:10 -0500

    añadiendo vista ingreso atractivo

---
+++ app/src/main/java/com/example/ppmoviles/IngresoAtractivo.kt
@@ -0,0 +1,11 @@
+package com.example.ppmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class IngresoAtractivo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingreso_atractivo)
+    }
+}
