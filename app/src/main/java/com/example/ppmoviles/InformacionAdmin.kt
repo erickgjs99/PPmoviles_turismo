c896c941d43cfe4a0dbfb00a5a141cc689f774db
Erick Gonzalo Jaramillo Soto <erickjs99lol@gmail.com>
2022-01-15 19:02:40 -0500

    añadiendo vistas de clima y administrador

---
+++ app/src/main/java/com/example/ppmoviles/InformacionAdmin.kt
@@ -0,0 +1,11 @@
+package com.example.ppmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InformacionAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_informacion_admin)
+    }
+}
