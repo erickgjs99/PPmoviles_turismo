d659a4eb7075da1584d973d3c15167a26d4d90dc
Erick Gonzalo Jaramillo Soto <erickjs99lol@gmail.com>
2022-01-24 21:47:26 -0500

    subiendo formulario de higiene y seguridad

--- app/src/main/java/com/example/ppmoviles/HigieneSeguridadTuristica.kt
+++ app/src/main/java/com/example/ppmoviles/HigieneSeguridadTuristica.kt
@@ -1,11 +1,17 @@
 package com.example.ppmoviles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_higiene_seguridad_turistica.*
 
 class HigieneSeguridadTuristica : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_higiene_seguridad_turistica)
+
+        btn_regresarHigieneTuristica.setOnClickListener(){
+            startActivity(Intent(this, OptionsConservacionAtractivo::class.java))
+        }
     }
 }
